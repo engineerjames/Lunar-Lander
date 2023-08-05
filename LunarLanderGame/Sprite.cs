2ed9110a598514023baa167015457c7462e76b16
James Armes <jamesleearmes@gmail.com>
2023-08-05 09:58:21 -0600

    Add basic Sprite class that our game specific objects can derive from

---
+++ LunarLanderGame/Sprite.cs
@@ -0,0 +1,90 @@
+﻿namespace LunarLanderGame
+{
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework;
+    using System;
+
+    public class Sprite : DrawableGameComponent
+    {
+        private static float DEG_TO_RADIANS = (float)( Math.PI / 180.0 );
+
+        private Texture2D texture;
+        private Vector2 position;
+        private Color color;
+        private float rotation;
+        private Vector2 origin;
+        private Vector2 scale;
+
+        public Sprite(Game game, Texture2D texture, Vector2 position)
+            : base(game)
+        {
+            this.texture = texture;
+            this.position = position;
+            color = Color.White;
+            rotation = 0f;
+
+            // Default to the center
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
+
+            scale = Vector2.One;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = Game.Services.GetService<SpriteBatch>();
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position, null, color, rotation, origin, scale, SpriteEffects.None, 0f);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        // Helper methods to manipulate the sprite's properties
+        public void SetPosition(Vector2 newPosition)
+        {
+            position = newPosition;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return position;
+        }
+
+        public void SetColor(Color newColor)
+        {
+            color = newColor;
+        }
+
+        public Color GetColor()
+        {
+            return color;
+        }
+
+        public void SetRotation(float newRotationDegrees)
+        {
+            // Internally the rotation is stored in radians, but we expect users to
+            // give us the rotation in units of degrees
+            rotation = newRotationDegrees * DEG_TO_RADIANS;
+        }
+
+        public float GetRotation()
+        {
+            return rotation;
+        }
+
+        public void SetOrigin(Vector2 newOrigin)
+        {
+            origin = newOrigin;
+        }
+
+        public Vector2 GetOrigin()
+        {
+            return origin;
+        }
+
+        public void SetScale(Vector2 newScale)
+        {
+            scale = newScale;
+        }
+    }
+}
