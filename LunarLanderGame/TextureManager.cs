e21d6fa9ca47d2a6eca928d6ae9c8b5f377f50c0
James Armes <jamesleearmes@gmail.com>
2023-08-05 09:57:54 -0600

    Add basic class for managing texture loading

---
+++ LunarLanderGame/TextureManager.cs
@@ -0,0 +1,61 @@
+﻿namespace LunarLanderGame
+{
+    using LunarLanderGame.Logging;
+
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+
+    using System.Collections.Generic;
+
+    internal class TextureManager
+    {
+        private ContentManager contentManager;
+        private Dictionary<string, Texture2D> textures;
+        private ILogger logger;
+
+        public TextureManager(ContentManager contentManager, ILogger logger)
+        {
+            this.contentManager = contentManager;
+            textures = new Dictionary<string, Texture2D>();
+            this.logger = logger;
+        }
+
+        // Load a single texture and store it in the dictionary
+        private void LoadTexture(string assetName)
+        {
+            if (!textures.ContainsKey(assetName))
+            {
+                Texture2D texture = contentManager.Load<Texture2D>(assetName);
+                textures.Add(assetName, texture);
+            }
+        }
+
+        // Load all textures from the content file
+        public void LoadAllTextures()
+        {
+            // Add all your texture asset names here -- TODO: Load from input file?
+            string[] textureAssetNames = new string[]
+            {
+                "lander"
+            };
+
+            foreach (string assetName in textureAssetNames)
+            {
+                LoadTexture(assetName);
+            }
+        }
+
+        // Retrieve a texture by its asset name
+        public Texture2D GetTexture(string assetName)
+        {
+            if (textures.TryGetValue(assetName, out Texture2D texture))
+            {
+                return texture;
+            }
+
+            // If the texture is not found, you can handle it here (e.g., log a warning)
+            logger.Log(ILogger.LogLevel.Error, $"Unable to find texture with assetName: {assetName}");
+            return null;
+        }
+    }
+}
