b382fa3ec349d6293c642af366ea2b1993bce3f6
James Armes <jamesleearmes@gmail.com>
2023-08-05 09:57:38 -0600

    Add basic logging components

---
+++ LunarLanderGame/Logging/ConsoleLogger.cs
@@ -0,0 +1,36 @@
+﻿namespace LunarLanderGame.Logging
+{
+    using System.Diagnostics;
+
+    using static LunarLanderGame.Logging.ILogger;
+
+
+    internal class ConsoleLogger : ILogger
+    {
+        public void Log(LogLevel level, string message)
+        {
+            string logLevelString = GetLogLevelString(level);
+            Debug.WriteLine($"[{logLevelString}] {message}");
+        }
+
+        private string GetLogLevelString(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Debug:
+                    return "DEBUG";
+                case LogLevel.Info:
+                    return "INFO";
+                case LogLevel.Warning:
+                    return "WARNING";
+                case LogLevel.Error:
+                    return "ERROR";
+                case LogLevel.Fatal:
+                    return "FATAL";
+                default:
+                    return "UNKNOWN";
+            }
+        }
+    }
+
+}
---
+++ LunarLanderGame/Logging/ILogger.cs
@@ -0,0 +1,18 @@
+﻿namespace LunarLanderGame.Logging
+{
+    internal interface ILogger
+    {
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Warning,
+            Error,
+            Fatal
+        }
+
+
+        void Log(LogLevel level, string message);
+
+    }
+}
